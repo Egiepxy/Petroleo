5ac0a7d92f60de83ba28ef7a8b2a6c04f75dbcca
Egiepxy <epxy@hotmail.com>
2026-09-15 08:30:54 -0300

    Correct syntax error in yahooHistory function
    Fix syntax error in URL construction for Yahoo API.

--- server.js
+++ server.js
@@ -106,7 +106,10 @@ async function yahooHistory(tf) {
   const cfg = yahooConfig(tf);
 
   const url =
+   server.js:109
     ${YAHOO}/${encodeURIComponent(YAHOO_SYMBOL)} +
+    ^
+SyntaxError: Unexpected token '{'
     ?range=${cfg.range} +
     &interval=${cfg.interval} +
     &includePrePost=false +
